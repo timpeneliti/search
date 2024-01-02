5c21165557df1ad9d6cee3fe0d2e235036a25b24
adezayd <120370323+adezayd@users.noreply.github.com>
2024-01-02 17:16:57 +0700

    Initial commit

---
+++ search_form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Search Engine</title>
+</head>
+<body>
+    <form action="search_results.php" method="get">
+        <label for="search">Search:</label>
+        <input type="text" name="search" id="search">
+        <input type="submit" value="Search">
+    </form>
+</body>
+</html>
